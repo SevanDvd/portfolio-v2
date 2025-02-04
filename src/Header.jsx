69f79a53e8d3ce8900df51c293ee120e88531576
SevanDvd <sevan.david2a@gmail.com>
2025-02-04 19:01:39 +0100

    header creation

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+function Header(){
+
+    return (
+        <header style={styles.header}>
+            are u here
+        </header>
+    )
+}
+
+const styles = {
+    header: {
+        top: 0,
+        width: "100%"
+    }
+}
+
+export default Header
