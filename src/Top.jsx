75028929815393d58d658de83147eb84c7baf009
SevanDvd <sevan.david2a@gmail.com>
2025-02-26 09:38:13 +0100

    header update

--- src/Top.jsx
+++ src/Top.jsx
@@ -5,12 +5,28 @@ function Top(){
 
     return (
         <>
-            <header className="w-full h-24 flex items-center">
+            <header className="w-full h-24 flex items-center justify-between">
 
-                <div href="" className="w-44 h-12 cursor-pointer hover:bg-[#00000040] border-4 border-black">
+                <div href="" className="w-44 h-12 cursor-pointer hover:bg-[#00000040] border-4 border-black ml-4">
                     <p className="font-[Retron2000] text-black text-2xl m-1">
                     Sevan David
                     </p>
+                </div>
+
+                <div className="w-61 h-12 flex mr-4 ">
+
+                    <div className="w-45 h-12 cursor-pointer border-4 border-black ml-4 bg-white">
+                        <p className="font-[Retron2000] text-black text-2xl m-1">
+                        Projets &nbsp; &nbsp; V
+                        </p>
+                    </div>
+
+                    <div className="w-14 h-12 cursor-pointer hover:bg-[#00000040] border-4 border-black -m-1">
+                        <p className="font-[Retron2000] text-black text-2xl m-1">
+                        Moi
+                        </p>
+                    </div>
+
                 </div>
                 
             </header>
